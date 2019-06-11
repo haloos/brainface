e44ce5cde01abbf43e5bf98e9f1de91286f230c5
Jason <haloos@me.com>
2019-06-11 09:39:43 -0400

    added onInpuChange

--- src/App.js
+++ src/App.js
@@ -18,7 +18,16 @@ const particlesOptions = {
   }
 }
 
-class App extends Component {
+class App extends Component { 
+  constructor() {
+    super() ;
+    this.state ={
+      input: '', 
+    }
+  } 
+  onInputChange = (event) => {
+    console.log(event);
+  }
   render() {
     return ( 
       <div className="App">
