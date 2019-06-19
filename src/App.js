1480c947c07375f7c09f9fc10556d8b0de6a02f7
Jason <haloos@me.com>
2019-06-19 10:30:38 -0400

    updated axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component }  from 'react'; 
 import Navigation from './components/Navigation/Navigation'; 
+import Clarifai  from 'clarifai';
 import Logo from './components/Logo/Logo'; 
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'; 
 import Rank from './components/Rank/Rank'; 
 import Particles from 'react-particles-js';
-import './App.css'; 
+import './App.css';  
 
 const app = new Clarifai.App({
   apiKey:'573626925c2e4086992618c56bc6d703'
@@ -22,35 +23,48 @@ const particlesOptions = {
   }
 }
 
-class App extends Component { 
+class App extends Component{ 
   constructor() {
     super();
     this.state = {
       input: '', 
     }
   } 
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    console.log(event.target.value); 
   } 
 
-  onButtonSubmit = () => { 
-   console.log('click'); 
+  onButtonSubmit = () => {
+    console.log('click'); 
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+     console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+    
   }
+
   render() {
-    return ( 
-      <div className="App">
-        <Particles  className='particles' 
-          parms={particlesOptions}
-       />
-        <Navigation/> 
+    return (
+      <div className="App"> 
+      <Particles 
+      params={particlesOptions}
+      />
+      <Navigation/> 
         <Logo/> 
         <Rank/>
         <ImageLinkForm 
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}/> 
-        {/*<FaceRecognition/>*/}
-    </div>
-  );
-};
-}
-export default App;
+        {/*<FaceRecognition/>*/} 
+      </div>
+
+    )
+  }
+}  
+
+export default App; 
