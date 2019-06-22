ea50b4908fa5f9e888f5d07344e78d7944e2b0c8
Jason <haloos@me.com>
2019-06-22 11:41:46 -0400

    added calculateFaceLocation

--- src/App.js
+++ src/App.js
@@ -32,6 +32,9 @@ class App extends Component{
       imageUrl:'',
     }
   } 
+  calculateFaceLocation = (data) => {
+
+  } 
 
   onInputChange = (event) => {
     this.setState({input: event.target.value}); 
@@ -43,13 +46,9 @@ class App extends Component{
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(
-    function(response) {
-     console.log(response.outputs[0].data.regions[0].region_info.bounding_box); 
-    },
-    function(err) {
-      // there was an error
-    }
+    .then(response => this.calculateFaceLocation(response)
+    .catch(err => console.log(err))
+      
   );
     
   }
